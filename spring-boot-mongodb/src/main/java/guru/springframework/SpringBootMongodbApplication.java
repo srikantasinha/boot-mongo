a7d5f2a76d994175bb8b45ac849b745cc9c95aca
srikantasinha <srikantasinha@gmail.com>
2018-10-07 19:09:44 +0530

    Adding an update to GitHub.

--- spring-boot-mongodb/src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ spring-boot-mongodb/src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -3,6 +3,8 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test Mongo DB project
+
 @SpringBootApplication
 public class SpringBootMongodbApplication {
 
