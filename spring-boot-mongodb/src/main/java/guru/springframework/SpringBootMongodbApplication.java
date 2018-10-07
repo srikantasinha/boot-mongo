1c4ea716be0b5d861e98dc8cd028e166c81b6187
srikantasinha <srikantasinha@gmail.com>
2018-10-07 19:25:39 +0530

    Update SpringBootMongodbApplication.java
    GH update

--- spring-boot-mongodb/src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ spring-boot-mongodb/src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -3,7 +3,7 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test Mongo DB project -2
+//Test Mongo DB project -3
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
